58f94a413fecdef78dd9e891d9b577411d24ff1c
Giacomo DR <delrig@usi.ch>
2018-02-11 17:00:29 +0100

    Refactor main-maptest

--- src/main-maptest.cpp
+++ src/main-maptest.cpp
@@ -57,6 +57,24 @@ void generateThresholdMap( HeightMap& map, double treshold, Point2D step, std::s
     saveImage( filename.c_str(), image, img_width, img_height );
 }
 
+void generateProbabilityMap( TraversabilityGraph& tg, double angle, std::string filename ) {
+    unsigned img_width = 64*10, img_height = 64*10;
+    std::vector<unsigned char> image;
+    image.resize(img_width * img_height * 4); // 8-bit RGBA
+
+    for( unsigned y = 0 ; y < img_height ; y++ )
+        for( unsigned x = 0 ; x < img_width ; x++ ){
+            double xd = (x / 640.0) * 63.0;
+            double yd = (y / 640.0) * 63.0;
+            unsigned char val = static_cast<unsigned char>( tg.getLinear( xd, yd, angle ) * 255 );
+            image[4 * img_width * y + 4 * x + 0] = val;
+            image[4 * img_width * y + 4 * x + 1] = val;
+            image[4 * img_width * y + 4 * x + 2] = val;
+            image[4 * img_width * y + 4 * x + 3] = 255;
+        }
+    saveImage( filename.c_str(), image, img_width, img_height );
+}
+
 int main() {
 
     TraversabilityGraph tg( TG_SIZE, TG_SIZE );
@@ -68,6 +86,8 @@ int main() {
     generateThresholdMap( map, 0.8, Point2D(-0.1, 0.0), "tgraphs/tgraph_tv_left.png" );
     generateThresholdMap( map, 0.8, Point2D(0.0, 0.1), "tgraphs/tgraph_tv_up.png" );
     generateThresholdMap( map, 0.8, Point2D(0.0, -0.1), "tgraphs/tgraph_tv_down.png" );
+
+    generateProbabilityMap( tg, 0.0, "tgraphs/tgraph_0.png" );
 
 //    std::cout << tg.get( 1, 1, 0 ) << std::endl;
 //    std::cout << tg.get( 1, 1, 1 ) << std::endl;
@@ -87,55 +107,6 @@ int main() {
 //    std::cout << tg.getLinear( 1.0, 1.0, 3.0/2.0*M_PI ) << std::endl;
 //    std::cout << tg.getLinear( 1.0, 1.0, M_PI / 4.0 ) << std::endl;
 //    std::cout << tg.getLinear( 1.0, 1.0, M_PI / 8.0 ) << std::endl;
-
-    // Generate an series of images of tg with various angles
-//    unsigned width = 64*10, height = 64*10;
-//    std::vector<unsigned char> image;
-//    image.resize(width * height * 4);
-//    int i = 0;
-//    for( double angle = 0 ; angle < M_PI * 2.0 ; i++, angle += M_PI / 2.0 ){
-//        for (unsigned y = 0; y < height; y++)
-//            for (unsigned x = 0; x < width; x++) {
-//                double xd = (x / 640.0) * 63.0;
-//                double yd = (y / 640.0) * 63.0;
-//
-//                //std::cout << xd << "," << yd << std::endl;
-//                unsigned char val = (unsigned char)(tg.getLinear( xd, yd, angle ) * 255);
-//                image[4 * width * y + 4 * x + 0] = val;
-//                image[4 * width * y + 4 * x + 1] = val;
-//                image[4 * width * y + 4 * x + 2] = val;
-//                image[4 * width * y + 4 * x + 3] = 255;
-//            }
-//        std::ostringstream fname;
-//        fname << "tgraphs/tgraph_" << std::setfill('0') << std::setw(4) << i << ".png";
-//        std::cout << fname.str() << std::endl;
-//        encodeOneStep( fname.str().c_str(), image, width, height );
-//        std::cout << angle << std::endl;
-//    }
-//
-//    // Do the same as before but querying map instead of tg
-//    image.resize(width * height * 4);
-//    i = 0;
-//    for( double angle = 0 ; angle < M_PI * 2.0 ; i++, angle += M_PI / 2.0 ){
-//        for (unsigned y = 0; y < height; y++)
-//            for (unsigned x = 0; x < width; x++) {
-//                double xd = (x / 640.0) * map.size_x_mt();
-//                double yd = (y / 640.0) * map.size_y_mt();
-//
-//                //std::cout << xd << "," << yd << std::endl;
-//                unsigned char val = (unsigned char)
-//                        (map.traversability_prob( MapOrigin::TOP_LEFT, xd, yd, angle ) * 255);
-//                image[4 * width * y + 4 * x + 0] = val;
-//                image[4 * width * y + 4 * x + 1] = val;
-//                image[4 * width * y + 4 * x + 2] = val;
-//                image[4 * width * y + 4 * x + 3] = 255;
-//            }
-//        std::ostringstream fname;
-//        fname << "tgraphs/tgraph_" << std::setfill('0') << std::setw(4) << i << "_map.png";
-//        std::cout << fname.str() << std::endl;
-//        encodeOneStep( fname.str().c_str(), image, width, height );
-//        std::cout << angle << std::endl;
-//    }
 
     return 0;
 }
